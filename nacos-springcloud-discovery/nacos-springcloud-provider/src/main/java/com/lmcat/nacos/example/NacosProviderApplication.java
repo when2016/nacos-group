e87c3a434e8644cfe8f37373823d62bac1266e79
wanghongen <329016414@qq.com>
2019-07-14 16:02:40 +0800

    provider

---
+++ nacos-springcloud-discovery/nacos-springcloud-provider/src/main/java/com/lmcat/nacos/example/NacosProviderApplication.java
@@ -0,0 +1,15 @@
+package com.lmcat.nacos.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动配置管理
+ */
+@SpringBootApplication
+public class SpringcloudProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudProviderApplication.class, args);
+    }
+}
