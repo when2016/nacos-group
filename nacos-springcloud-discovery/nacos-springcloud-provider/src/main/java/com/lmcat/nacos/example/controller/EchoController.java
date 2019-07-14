59d558e27c9cef40bb046ecdbffcbef0c436fe0a
wanghongen <329016414@qq.com>
2019-07-14 16:50:36 +0800

    adddddddddddd

--- nacos-springcloud-discovery/nacos-springcloud-provider/src/main/java/com/lmcat/nacos/example/controller/EchoController.java
+++ nacos-springcloud-discovery/nacos-springcloud-provider/src/main/java/com/lmcat/nacos/example/controller/EchoController.java
@@ -12,6 +12,9 @@ public class EchoController {
     @RequestMapping(value = "echo/{string}", method = RequestMethod.GET)
     public String echo(@PathVariable String string) {
         System.out.println("string====" + string);
+        System.out.println("string====" + string);
+        System.out.println("string====" + string);
+        System.out.println("string====" + string);
         return "Hello Nacos Discovery " + string;
     }
 }
