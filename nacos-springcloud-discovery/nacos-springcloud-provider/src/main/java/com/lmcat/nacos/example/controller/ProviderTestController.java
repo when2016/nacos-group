53229cf86809989db71c7e70c01c3e7e0c24c7e2
wanghongen <329016414@qq.com>
2019-07-14 17:19:33 +0800

    éè¿spring cloud commonä¸­çè´è½½åè¡¡æ¥å£éåæå¡æä¾èç¹å®ç°æ¥å£è°ç¨         ServiceInstance serviceInstance = 	loadBalancerClient.choose("service-provider");

---
+++ nacos-springcloud-discovery/nacos-springcloud-provider/src/main/java/com/lmcat/nacos/example/controller/ProviderTestController.java
@@ -0,0 +1,20 @@
+package com.lmcat.nacos.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+@Slf4j
+public class ProviderTestController {
+
+    @GetMapping("hello")
+    public String hello(@RequestParam String name) {
+        log.info("invoked name = " + name);
+        return "Hello Nacos Discovery " + name;
+    }
+}
+
