c0c25cecdec15bb2a459a1f0e406c2d75c82083e
wanghongen <329016414@qq.com>
2019-07-14 17:35:02 +0800

    ssss

--- nacos-springcloud-discovery/nacos-springcloud-consumer/src/main/java/com/lmcat/nacos/example/NacosConsumerApplication.java
+++ nacos-springcloud-discovery/nacos-springcloud-consumer/src/main/java/com/lmcat/nacos/example/NacosConsumerApplication.java
@@ -3,9 +3,6 @@ package com.lmcat.nacos.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * 启动配置管理
@@ -14,11 +11,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 public class NacosConsumerApplication {
 
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(NacosConsumerApplication.class, args);
--- nacos-springcloud-discovery/nacos-springcloud-provider/src/main/java/com/lmcat/nacos/example/NacosProviderApplication.java
+++ nacos-springcloud-discovery/nacos-springcloud-provider/src/main/java/com/lmcat/nacos/example/NacosProviderApplication.java
@@ -14,4 +14,5 @@ public class NacosProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosProviderApplication.class, args);
     }
+
 }
