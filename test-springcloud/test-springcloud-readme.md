7667bcd74b2daca751b80ea6934ce91a2ad7190e
wanghongen <329016414@qq.com>
2019-07-26 19:30:36 +0800

    add

---
+++ test-springcloud/test-springcloud-readme.md
@@ -0,0 +1,3 @@
+
+###参考文档
+https://segmentfault.com/a/1190000019074715
---
+++ test-springcloud/test-springcloud-userservice/src/main/java/com/lmcat/test/example/UserServiceApplication.java
@@ -0,0 +1,39 @@
+package com.lmcat.test.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author：恒宇少年 - 于起宇
+ * <p>
+ * DateTime：2019-05-05 11:40
+ * Blog：http://blog.yuqiyu.com
+ * WebSite：http://www.jianshu.com/u/092df3f77bca
+ * Gitee：https://gitee.com/hengboy
+ * GitHub：https://github.com/hengboy
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class UserServiceApplication {
+    /**
+     * logger instance
+     */
+    static Logger logger = LoggerFactory.getLogger(UserServiceApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+        logger.info("「「「「「用户服务启动完成.」」」」」");
+    }
+
+    @GetMapping(value = "/index")
+    public String index() {
+        System.out.println("index(---port=9090).....");
+        return "this is user index---port=9090";
+    }
+}
